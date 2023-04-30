9f4d02480b56bc877c7c1db44b2d9d2c95cab7aa
Angel Montoya <j.gustavo.m245@gmail.com>
2023-04-30 13:28:45 -0600

    optimized code navbar - color

--- src/pages/Navbar.tsx
+++ src/pages/Navbar.tsx
@@ -1,4 +1,3 @@
-import { Outlet, Link } from "react-router-dom";
 import {
   RiGithubLine,
   RiTwitterLine,
@@ -10,61 +9,45 @@ import {
   TiInfoLargeOutline,
   TiHomeOutline,
 } from "react-icons/ti";
+
+import { Outlet, Link } from "react-router-dom";
 import { useState } from "react";
+
 function Navbar() {
-  const [dir, setDir] = useState(() => {
-    const path = window.location.pathname;
-    if (path === "/skills") {
-      return "Skills";
-    } else if (path === "/projects") {
-      return "Projects";
-    } else {
-      return "Home";
-    }
-  });
+  const paths: { [key: string]: string } = {
+    "/skills": "Skills",
+    "/projects": "Projects",
+    "/": "Home",
+  };
+  const [dir, setDir] = useState(
+    () => paths[window.location.pathname] || "Home"
+  );
+
+  let active = "text-red-500 flex items-center gap-2";
+  let inactive = "fill-white flex items-center gap-2";
 
   return (
     <div className="flex flex-col h-screen">
       <div className="flex bg-[#191724] text-white rounded-xl h-12 my-5 mx-12 ">
         <nav className="flex items-center justify-start gap-10 px-10 w-1/2">
           <Link to="/" onClick={() => setDir("Home")}>
-            <p
-              className={
-                dir === "Home"
-                  ? "text-red-500 flex items-center gap-2"
-                  : "fill-white flex items-center gap-2"
-              }
-            >
-              <TiHomeOutline
-                className={dir === "Home" ? "fill-red-500" : "fill-white"}
-              />
+            <p className={dir === "Home" ? active : inactive}>
+              <TiHomeOutline className={dir === "Home" ? active : inactive} />
               Home
             </p>
           </Link>
           <Link to="/skills" onClick={() => setDir("Skills")}>
-            <p
-              className={
-                dir === "Skills"
-                  ? "text-red-500 flex items-center gap-2"
-                  : "fill-white flex items-center gap-2"
-              }
-            >
+            <p className={dir === "Skills" ? active : inactive}>
               <TiInfoLargeOutline
-                className={dir === "Skills" ? "fill-red-500" : "fill-white"}
+                className={dir === "Skills" ? active : inactive}
               />
               Skills
             </p>
           </Link>
           <Link to="/projects" onClick={() => setDir("Projects")}>
-            <p
-              className={
-                dir === "Projects"
-                  ? "text-red-500 flex items-center gap-2"
-                  : "fill-white flex items-center gap-2"
-              }
-            >
+            <p className={dir === "Projects" ? active : inactive}>
               <TiFolderOpen
-                className={dir === "Projects" ? "fill-red-500" : "fill-white"}
+                className={dir === "Projects" ? active : inactive}
               />
               Projects
             </p>
