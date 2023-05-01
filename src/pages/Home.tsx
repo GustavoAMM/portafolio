0588953da2dc0235f714e8f8a420ce317efdb6a2
Angel Montoya <j.gustavo.m245@gmail.com>
2023-05-01 15:22:48 -0600

    responsive slidebar - home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,11 +3,11 @@ import Slidebar from "../Components/Home/Components/Slidebar";
 import Curriculum from "../Components/Home/Components/Curriculum";
 function Home() {
   return (
-    <div className="w-full xl:flex sm:flex grid mx-4">
+    <div className="w-full xl:flex sm:flex grid justify-items-center mx-4">
       <div className="xl:w-3/4 sm:w-3/4 m-4 ">
         <Main />
       </div>
-      <div className="w-1/4 m-4 grid gap-4 content-around">
+      <div className="xl:w-1/4 md:w-[300px] w-[250px] grid gap-4 m-4 content-around">
         <Slidebar />
         <Curriculum />
       </div>
