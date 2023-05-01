7763cf9067c7e61fc8e6b172abe265c9bdd88488
Angel Montoya <j.gustavo.m245@gmail.com>
2023-05-01 15:29:11 -0600

    responsive tools - skills

--- src/Components/Skills/Components/Tools.tsx
+++ src/Components/Skills/Components/Tools.tsx
@@ -5,7 +5,7 @@ function Tools() {
   return (
     <div className="grid gap-5">
       {skillsData.map(({ name, items }) => (
-        <div className="flex gap-4" key={name}>
+        <div className="xl:flex grid grid-cols-2 xl:my-0 my-4 gap-4" key={name}>
           <Iconos data={items} />
         </div>
       ))}
